67d4d69893b93b82f2d34c69b54a60f640bb58dd
harry <ssaamm11223363@gmail.com>
2023-11-12 11:18:39 +0800

    Initial commit

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
-from flask import Flask, render_template, request, redirect, jsonify
-import base64
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import text
-import csv
 import subprocess
 import pandas as pd
-from datetime import datetime
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 from flask_socketio import SocketIO
-from flask import Flask, render_template, request  # flask裡要抓前端用的
-from keras.models import load_model  # 跟keras有關的都是模型要用的
-from keras.utils import load_img, img_to_array
+from keras.models import load_model
 import keras.utils as image
-import numpy as np  # 模型機率要用的
+import numpy as np
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -26,36 +19,42 @@ app.config[
     'SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}?charset=utf8"
 db = SQLAlchemy(app)
 
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-    chines = db.Column(db.Integer)
-    math = db.Column(db.Integer)
-    english = db.Column(db.Integer)
-
-
 class Price(db.Model):
     id = db.Column(db.String(80), nullable=False, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     price_up = db.Column(db.Integer)
     price_down = db.Column(db.Integer)
 
+class Price2(db.Model):
+    id = db.Column(db.String(80), nullable=False, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    price_L1 = db.Column(db.Integer)
+    price_L2 = db.Column(db.Integer)
+    price_L3 = db.Column(db.Integer)
+    price_L4 = db.Column(db.Integer)
+    price_L5 = db.Column(db.Integer)
+    price_L6 = db.Column(db.Integer)
+    price_L7 = db.Column(db.Integer)
 
-# 資料庫初始化
+
+# 資料庫初始
 with app.app_context():
     db.create_all()
-
 
 @app.route('/')
 def aaa():
     return render_template('admin.html')
 
 
-@app.route('/search')
+@app.route('/search', methods=['GET', 'POST'])
 def search():
-    price = Price.query.all()
-    return render_template('search.html', price=price)
+    if request.method == 'POST':
+        search_term = request.form['searchInput']
+        results = Price2.query.filter_by(name=search_term).all()
+        return render_template('search.html', price=results)
+    else:
+        price = Price2.query.all()
+        return render_template('search.html', price=price)
 
 
 #############價格抓取###############
@@ -63,67 +62,60 @@ def search():
 def price_catch():
     try:
         # 使用subprocess执行price_catch.py脚本
-        subprocess.run(["python", "price_catch.py"], check=True)
+        subprocess.run(["python", "price_catch2.py"], check=True)
         return redirect('/price_import')
     except subprocess.CalledProcessError as e:
         return f"price_catch.py 腳本執行失敗，錯誤訊息：{e}"
-
 
 ##############價格導入覆蓋################
 @app.route('/price_import')
 def price_import():
     file_path = 'D:/專題/student/price.csv'
-    # 读取CSV文件
     df = pd.read_csv(file_path, encoding='utf-8')
-    for index, row in df.iterrows():
-        id, name, price_up, price_down = row
 
-        if not pd.isnull(id):  # 檢查id是否有效
-            existing_price = Price.query.filter_by(id=id).first()  # 只有在id有效時才進行查詢
+    for index, row in df.iterrows():
+        id, name, price_L1 = row
+
+        if not pd.isnull(id):
+            existing_price = Price2.query.filter_by(id=id).first()
 
             if existing_price:
-                # 如果记录已存在，覆盖现有数据
+                # 移動數據
+                existing_price.price_L7 = existing_price.price_L6
+                existing_price.price_L6 = existing_price.price_L5
+                existing_price.price_L5 = existing_price.price_L4
+                existing_price.price_L4 = existing_price.price_L3
+                existing_price.price_L3 = existing_price.price_L2
+                existing_price.price_L2 = existing_price.price_L1
+
+                # 更新紀錄
                 existing_price.name = name
-                existing_price.price_up = price_up
-                existing_price.price_down = price_down
+                existing_price.price_L1 = price_L1
             else:
-                # 如果记录不存在，创建新记录
-                new_price_entry = Price(id=id, name=name, price_up=price_up, price_down=price_down)
+                # 如果沒有紀錄，創新
+                new_price_entry = Price2(
+                    id=id,
+                    name=name,
+                    price_L1=price_L1,
+                    price_L2=None,
+                    price_L3=None,
+                    price_L4=None,
+                    price_L5=None,
+                    price_L6=None,
+                    price_L7=None,
+                )
                 db.session.add(new_price_entry)
 
     db.session.commit()
-
-    return "價格導入成功"
-
-
-
-@app.route('/identify')
-def identify():
-    return render_template('identify.html')
-
+    print("價格導入資料庫成功")
+    return ("價格導入資料庫成功")
 
 ######################蔬菜辨識##############################
-'''
-category={
-    0: 'FA1黃秋葵', 1: 'FB花椰菜', 2: 'FC胡瓜', 3: 'FD花胡瓜', 4: 'FE冬瓜',
-    5: 'FF絲瓜', 6: 'FG苦瓜', 7: 'FH扁蒲', 8: 'FI茄子', 9: 'FJ番茄',
-    10: 'FK甜椒', 11: 'FL豌豆', 12: 'FM菜豆', 13: 'FN敏豆', 14: 'FP萊豆',
-    15: 'FQ毛豆', 16: 'FR青花苔', 17: 'FS越瓜', 18: 'FT南瓜', 19: 'FU3石蓮花',
-    20: 'FU隼人瓜', 21: 'FV辣椒', 22: 'FW2洛神花', 23: 'FY玉米', 24: 'FZ落花生',
-    25: 'LA甘藍', 26: 'LB小白菜', 27: 'LC包心白', 28: 'LD青江白菜', 29: 'LE皇宮菜',
-    30: 'LF蕹菜', 31: 'LG芹菜', 32: 'LH菠菜', 33: 'LI萵苣菜', 34: 'LJ芥菜',
-    35: 'LK芥藍菜', 36: 'LM莧菜', 37: 'LN油菜', 38: 'LO甘薯葉', 39: 'LP2九層塔',
-    40: 'LP3羅勒', 41: 'LP芫荽', 42: 'LQ紅鳳菜', 43: 'LS茴香', 44: 'LV巴西利',
-    45: 'LX蕨菜', 46: 'LY1西洋菜', 47: 'LY2黑甜仔菜', 48: 'LY3豬母菜', 49: 'LZ2藤川七',
-    50: 'MA洋菇', 51: 'MB草菇', 52: 'MC濕木耳', 53: 'MD濕香菇', 54: 'ME金絲菇',
-    55: 'MF蠔菇', 56: 'MG巴西蘑菇', 57: 'MI秀珍菇', 58: 'MJ杏鮑菇', 59: 'MK鴻禧菇',
-    60: 'ML珊瑚菇', 61: 'MN柳松菇', 62: 'SA蘿蔔', 63: 'SB胡蘿蔔', 64: 'SC馬鈴薯',
-    65: 'SD洋蔥', 66: 'SE青蔥', 67: 'SF韭菜', 68: 'SG大蒜', 69: 'SH竹筍',
-    70: 'SJ芋', 71: 'SK荸薺', 72: 'SL1豆薯', 73: 'SM牛蒡', 74: 'SN蓮藕',
-    75: 'SO甘薯', 76: 'SP薑', 77: 'SQ茭白筍', 78: 'SR菱角', 79: 'SU薯蕷',
-    80: 'SV蘆筍', 81: 'SW球莖甘藍', 82: 'SX芽菜類', 83: 'SZ4百合', 84: 'SZ7晚香玉筍'
-}
-'''
+@app.route('/identify')
+def identify():
+    price = Price2.query.filter_by().all
+    return render_template('identify.html',price=price)
+
 category = {
     0: 'FA1', 1: 'FB', 2: 'FC', 3: 'FD', 4: 'FE',
     5: 'FF', 6: 'FG', 7: 'FH', 8: 'FI', 9: 'FJ',
@@ -148,7 +140,7 @@ category = {
 model = load_model('D:/專題/student/model_senior_project_best_clear.h5')
 
 
-def predict_label(img_path):  # 預測函數
+def predict_label(img_path):
     try:
         # 實現圖像預處理，例如調整大小、正規化等
         i = image.load_img(img_path, target_size=(128, 128))
@@ -167,15 +159,6 @@ def predict_label(img_path):  # 預測函數
         print(f"辨識失敗或錯誤，請重新上傳其他照片: {e}")
         return "無法辨識圖像。 請嘗試使用不同的圖像."
 
-
-# routes設定
-@app.route("/", methods=['GET', 'POST'])
-def main():
-    # 將 "index.html" 模板呈現給用戶，顯示在用戶的瀏覽器中
-    return render_template("identify.html")
-
-
-# 按了傳送後的route位子
 @app.route("/submit", methods=['GET', 'POST'])
 def get_output():
     if request.method == 'POST':
@@ -184,31 +167,10 @@ def get_output():
         img.save(img_path)
         p = predict_label(img_path)
 
-        price_record = Price.query.filter_by(id=p).first()
+        price = Price2.query.filter_by(id=p).first()
 
-        if price_record:
-            price_name = price_record.name
-            price_up = price_record.price_up
-            price_down = price_record.price_down
-        else:
-            price_name = "N/A"
-            price_up = "N/A"
-            price_down = "N/A"
-
-        return render_template("identify.html", prediction=p, img_path=img_path, price_name=price_name,
-                               price_up=price_up, price_down=price_down)
-
-
+        return render_template("identify.html", prediction=p, img_path=img_path, price=price)
 ####################################################
-
-#########################時時辨識########################
-@app.route("/canmera", methods=['GET', 'POST'])
-def canmera():
-
-    return render_template("canmera.html")
-
-
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5050)
---
+++ price_catch2.py
@@ -0,0 +1,228 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from datetime import datetime, timedelta
+import time
+
+# 設定下載目錄
+options = webdriver.ChromeOptions()
+download_directory = "D:\專題\student"  # 將目錄路徑替換為您想要的下載目錄
+options.add_experimental_option("prefs", {
+    "download.default_directory": download_directory,
+    "download.prompt_for_download": False,
+    "download.directory_upgrade": True,
+    "safebrowsing.enabled": True
+})
+
+# 初始化瀏覽器
+driver = webdriver.Chrome(options=options)
+
+# 載入目標網站
+url = "https://amis.afa.gov.tw/veg/VegProdTransInfoCP.aspx"
+driver.get(url)
+
+# 等待3秒
+
+time.sleep(1)
+
+
+#市場
+handles = driver.window_handles
+driver.find_element(By.XPATH,"/html/body/form/table/tbody/tr[3]/td[2]/table/tbody/tr[2]/td/table/tbody/tr[2]/td[4]/textarea",).click()
+
+element3 = driver.find_element(By.XPATH, ('//*[@id="divDialog"]/iframe'))
+driver.switch_to.frame(element3)
+
+time.sleep(1)
+
+handles1 = driver.window_handles
+driver.find_element(By.XPATH,"/html/body/form/div[3]/table/tbody/tr[2]/td/select/option[2]",).click()
+
+time.sleep(1)
+
+driver.find_element(By.XPATH,"/html/body/form/div[3]/table/tbody/tr[3]/td/input",).click()
+driver.switch_to.window(handles[0])
+
+time.sleep(1)
+##############3 產品點選
+handles = driver.window_handles
+driver.find_element(By.XPATH,"/html/body/form/table/tbody/tr[3]/td[2]/table/tbody/tr[2]/td/table/tbody/tr[4]/td[4]/textarea",).click()
+
+time.sleep(1)
+
+element3 = driver.find_element(By.XPATH, ('//*[@id="divDialog"]/iframe'))
+driver.switch_to.frame(element3)
+
+handles1 = driver.window_handles
+driver.find_element(By.XPATH,"/html/body/form/div[4]/div/table/tbody/tr[1]/td/table/tbody/tr[2]/td[1]/input",).click()
+
+time.sleep(1)
+
+# 找到多選框元素，通常是一個<select>元素
+select_element = driver.find_element(By.XPATH, '/html/body/form/div[4]/div/table/tbody/tr[3]/td/select')
+
+# 使用Select物件包裝多選框元素
+from selenium.webdriver.support.ui import Select
+select = Select(select_element)
+
+# 開始迭代選項，從option[2]到option[128]
+for i in range(2, 129):
+    option_xpath = f'/html/body/form/div[4]/div/table/tbody/tr[3]/td/select/option[{i}]'
+    option_element = driver.find_element(By.XPATH, option_xpath)
+    select.select_by_value(option_element.get_attribute("value"))
+
+driver.find_element(By.XPATH,"/html/body/form/div[4]/div/table/tbody/tr[4]/td/input",).click()
+driver.switch_to.window(handles[0])
+
+handles1 = driver.window_handles
+driver.find_element(By.XPATH,"/html/body/form/table/tbody/tr[3]/td[2]/table/tbody/tr[2]/td/table/tbody/tr[7]/td/input[1]",).click()
+
+time.sleep(2)
+
+handles1 = driver.window_handles
+driver.find_element(By.XPATH,"/html/body/form/table/tbody/tr[3]/td[2]/table/tbody/tr[2]/td/table/tbody/tr[7]/td/input[2]",).click()
+
+time.sleep(2)
+
+
+# 最後，關閉瀏覽器
+driver.quit()
+
+import xlrd
+import openpyxl
+
+# 打開原始Excel檔案（使用xlrd來讀取舊的.xls格式）
+input_file = "蔬菜產品行情比較.xls"
+workbook = xlrd.open_workbook(input_file)
+sheet = workbook.sheet_by_index(0)
+
+# 要提取的欄位範圍
+columns_to_extract = [0, 2, 3]
+start_row = 6  # 注意：xlrd的索引從0開始，所以這裡使用7表示第8行
+end_row = sheet.nrows
+
+# 創建一個新的Excel檔案
+output_file = "output.xlsx"
+output_workbook = openpyxl.Workbook()
+output_sheet = output_workbook.active
+
+# 提取資料並寫入新的Excel檔案
+for row in range(start_row, end_row):
+    output_row = []
+    for column_index in columns_to_extract:
+        cell_value = sheet.cell_value(row, column_index)
+        output_row.append(cell_value)
+
+    output_sheet.append(output_row)
+
+# 保存新的Excel文件
+output_workbook.save(output_file)
+
+##############
+
+# 打开Excel文件
+input_file = "output.xlsx"
+output_file = "output.xlsx"  # 输出文件与输入文件相同
+
+# 打开工作簿
+workbook = openpyxl.load_workbook(input_file)
+sheet = workbook.active
+
+# 从B列和C列获取数据并四舍五入
+for row in sheet.iter_rows(min_row=1, min_col=2, max_col=3):
+    for cell in row:
+        if cell.value is not None:
+            cell.value = round(cell.value)  # 四舍五入为整数
+
+# 保存修改后的工作簿
+workbook.save(output_file)
+print("数据已四舍五入并保存回 output.xlsx")
+
+##################
+
+import openpyxl
+
+# 打开Excel文件
+input_file = "output.xlsx"
+output_file = "output.xlsx"  # 输出文件与输入文件相同
+
+# 打开工作簿
+workbook = openpyxl.load_workbook(input_file)
+sheet = workbook.active
+
+# 插入一列在A列之后
+sheet.insert_cols(2)  # 在A列之后插入一列
+
+# 复制A列的数据到新插入的列
+for row in sheet.iter_rows(min_row=1, max_row=sheet.max_row, min_col=1, max_col=1):
+    for cell in row:
+        if cell.value is not None:
+            new_cell = sheet.cell(row=cell.row, column=2, value=cell.value)
+
+# 保存修改后的工作簿
+workbook.save(output_file)
+print("已在A列之后插入一列，并复制A列的数据到新列")
+
+####################
+
+import openpyxl
+import re
+
+# 打开Excel文件
+input_file = "output.xlsx"
+output_file = "output.xlsx"  # 输出文件与输入文件相同
+
+# 打开工作簿
+workbook = openpyxl.load_workbook(input_file)
+sheet = workbook.active
+
+# 处理A列：保留英文和数字，删除中文
+for row in sheet.iter_rows(min_row=1, max_row=sheet.max_row, min_col=1, max_col=1):
+    for cell in row:
+        if cell.value is not None:
+            # 使用正则表达式匹配中文字符并删除
+            cell.value = re.sub(r'[\u4e00-\u9fff]', '', cell.value)
+
+# 处理B列：保留中文，删除英文和数字
+for row in sheet.iter_rows(min_row=1, max_row=sheet.max_row, min_col=2, max_col=2):
+    for cell in row:
+        if cell.value is not None:
+            # 使用正则表达式匹配英文和数字并删除
+            cell.value = re.sub(r'[a-zA-Z0-9]', '', cell.value)
+
+sheet.delete_cols(4)
+# 保存修改后的工作簿
+workbook.save(output_file)
+print("已处理A列和B列的数据")
+
+###
+
+import csv
+import openpyxl
+
+# 打开output.xlsx
+input_file = "output.xlsx"
+workbook = openpyxl.load_workbook(input_file)
+sheet = workbook.active
+
+# 创建一个CSV文件并打开以写入
+output_csv = "price.csv"
+with open(output_csv, 'w', newline='', encoding='utf-8') as csv_file:
+    csv_writer = csv.writer(csv_file)
+
+    # 遍历工作表中的行
+    for row in sheet.iter_rows(values_only=True):
+        # 使用列表推导式去除每个字段中的空格
+        cleaned_row = [str(cell).strip() for cell in row]
+        csv_writer.writerow(cleaned_row)
+
+print(f"数据已保存为 {output_csv}")
+
+import os
+
+file_to_delete1 = "蔬菜產品行情比較.xls"
+file_to_delete2 = "output.xlsx"
+
+os.remove(file_to_delete1)
+os.remove(file_to_delete2)
+
+
